e91f954d7f1d04d0466bb8d5effdb6298e2e4d3d
foeu <fabiano.junior@gft.com>
2021-01-05 15:19:39 -0300

    configuração de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: '/home', pathMatch: 'full' },
+  { path: "home", component: HomeComponent },
+  { path: "products", component: ProductCrudComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
